3df7216c964c3eb8fd797c946ecde0692336b7cb
RoundBearChoi <roundbeargames@gmail.com>
2021-01-11 19:06:54 +0900

    update UI according to each combination

--- PermuVis/Assets/PermsVisualized/Code/UI/UIController.cs
+++ PermuVis/Assets/PermsVisualized/Code/UI/UIController.cs
@@ -7,7 +7,6 @@ namespace Roundbeargames
     public class UIController : MonoBehaviour
     {
         [Header("---Setup---")]
-        public List<RectTransform> RowUIElements;
         public GameObject RowPrefab;
         public GameObject SelectorPrefab;
         public GameObject VerticalLinePrefab;
@@ -18,8 +17,9 @@ namespace Roundbeargames
         [SerializeField] float RowIndent = 0f;
 
         [Header("---Debug---")]
-        [SerializeField]
-        public GameLogic gameLogic;
+        [SerializeField] GameLogic gameLogic;
+        [SerializeField] List<RectTransform> RowUIList = new List<RectTransform>();
+        [SerializeField] List<RectTransform> SelectorUIList = new List<RectTransform>();
 
         public int TOTAL_ROWS
         {
@@ -37,21 +37,37 @@ namespace Roundbeargames
             }
         }
 
+        private void Update()
+        {
+            if (gameLogic != null)
+            {
+                for (int i = 0; i < gameLogic.TOTAL_ROWS; i++)
+                {
+                    if (i < SelectorUIList.Count && i < RowUIList.Count)
+                    {
+                        int totalItems = gameLogic.GetRow(i).listInts.Count;
+                        int selection = gameLogic.GetRow(i).selector.INDEX;
+                        float localX = GetLocalUIXPos(RowUIList[i], totalItems, selection);
+
+                        SelectorUIList[i].anchoredPosition = new Vector2(localX, 0f);
+                    }
+                }
+            }
+        }
+
         public void SetupGraphics(GameLogic logic)
         {
             gameLogic = logic;
+            SelectorUIList.Clear();
 
             for (int nRows = 0; nRows < gameLogic.TOTAL_ROWS; nRows++)
             {
                 RectTransform rect = CreateRowUI(nRows);
 
-                Row r = gameLogic.GetRow(nRows);
+                AttachSelector(nRows, rect);
 
-                for (int i = 0; i < r.listInts.Count ; i++)
-                {
-                    AttachSelector(rect);
-                    AttachVerticalLines(rect, r.listInts.Count);
-                }
+                Row r = gameLogic.GetRow(nRows);
+                AttachVerticalLines(rect, r.listInts.Count);
             }
         }
 
@@ -63,32 +79,42 @@ namespace Roundbeargames
             localP.y = yOffset * rowCount;
             AttachToRect(row, rowAnchor, localP);
 
+            RowUIList.Add(row.GetComponent<RectTransform>());
+
             return row.GetComponent<RectTransform>();
         }
 
-        private void AttachSelector(RectTransform rowRectTransform)
+        private void AttachSelector(int index, RectTransform rowRectTransform)
         {
             GameObject selectorObj = Instantiate(SelectorPrefab);
             AttachToRect(selectorObj, rowRectTransform, Vector2.zero);
+
+            SelectorUIList.Add(selectorObj.GetComponent<RectTransform>());
         }
 
         private void AttachVerticalLines(RectTransform rectTransform, int nCount)
         {
             for (int i = 0; i < nCount; i++)
             {
-                float leftside = -(float)rectTransform.rect.width / 2f;
-                float offset = 0;
-                
-                if (nCount > 1)
-                {
-                    offset = (float)rectTransform.rect.width / (nCount - 1);
-                }
-
-                Vector2 localPos = new Vector2(leftside + (offset * i), 0);
+                float localX = GetLocalUIXPos(rectTransform, nCount, i);
+                Vector2 localPos = new Vector2(localX, 0);
 
                 GameObject vertObj = Instantiate(VerticalLinePrefab);
                 AttachToRect(vertObj, rectTransform, localPos);
             }
+        }
+
+        float GetLocalUIXPos(RectTransform horizontalLine, int totalItems, int selection)
+        {
+            float leftside = -(float)horizontalLine.rect.width / 2f;
+            float offset = 0;
+
+            if (totalItems > 1)
+            {
+                offset = (float)horizontalLine.rect.width / (totalItems - 1);
+            }
+
+            return leftside + (offset * selection);
         }
 
         private void AttachToRect(GameObject obj, RectTransform parentRect, Vector2 localPos)
